f2d095a3e58aa510f76be8bcfc76508243b5e7bd
Radek Vlacil <radek.vlacil@gmail.com>
2015-10-21 12:18:03 +0200

    Move is now per screen as well

---
+++ .i3/move_to_position_per_output.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from get_workspaces import get_workspaces_on_current_output
+import subprocess
+import sys
+
+offset = int(sys.argv[1])
+
+workspaces = get_workspaces_on_current_output()
+real_offset = min(offset, len(workspaces) - 1)
+
+subprocess.call(['i3-msg', 'move container to workspace', workspaces[real_offset]], stdout=subprocess.PIPE)
