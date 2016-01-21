2b4925f29dc0791de392c6afde2a5fb643ced699
Radek Vlacil <radek.vlacil@gmail.com>
2016-01-21 10:36:51 +0100

    Adding moving to named workspace

---
+++ .i3/move_to_workspace_name.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from get_workspaces import get_workspaces_list
+import subprocess
+
+dmenu = subprocess.Popen(['dmenu', '-p', 'Workspaces'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+selected = dmenu.communicate('\n'.join(get_workspaces_list()))
+
+subprocess.call(['i3-msg', 'move container to workspace', selected[0].rstrip()], stdout=subprocess.PIPE)
